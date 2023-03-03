bb942644ead0f95410d974039b051527d3e909cd
panosgjuras <panosgjuras@gmail.com>
2023-03-03 17:59:29 +0200

    comb_procss file upload

--- comb_procss/four_shared_space_analysis.R
+++ comb_procss/four_shared_space_analysis.R
@@ -2,17 +2,19 @@ library(ggplot2)
 library(ggpubr)
 library(corrplot)
 library(gvlma)
-library(olsrr)
+# library(olsrr)
 library(RColorBrewer)
 library(psych)
 library(dplyr)
+library(rstudioapi)
 
-setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-gerdat<-read.csv2('datasets/datasets_germany_DESIGN.csv', header=T, dec=".", sep=",")
-eldat<-read.csv2('datasets/amalias_traffic_dataset_DESIGN.csv', header=T, dec=".", sep=",")
-
-# 
-
+#setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+setwd("C:/Users/panos/Desktop/github_tzouras/Shared_space_Traffic")
+# https://leopard.tu-braunschweig.de/receive/dbbs_mods_00071535
+gerdat<-read.csv2('lsaxony_procss/outdatasets/datasets_germany.csv', header=T, dec=".", sep=",")
+# https://data.mendeley.com/datasets/n3wzjd54pj
+eldat<-read.csv2('datasets/amalias_traffic_dataset.csv', header=T, dec=".", sep=",") # as it is downloaded from the data
+ 
 # NEW DATASET TO PLAY WITH
 datafr_fun <- function(df, x, dats, limit){
   if(dats == 'dats1'){datafr<-data.frame(case=x, flow=30*subset(df, case==x)$n_cars + 30*subset(df, case==x)$n_cyclists,
@@ -45,31 +47,31 @@ datafr_fun <- function(df, x, dats, limit){
                                          dcross = 30*subset(df, case==x)$cross - mean(30*subset(df, case==x)$cross),
                                          cross = 30*subset(df, case==x)$cross)}
   
-  datafr<-cbind(datafr, data.frame(shared = subset(df, case==x)$shared,
-                                    # 1, if shared space, only one section
-                                    oneway = subset(df, case==x)$oneway,
-                                    lanes = subset(df, case==x)$lanes,
-                                    # 1, if oneway road
-                                    # park = subset(df, case==x)$park, # no use, all section with parking
-                                    mark = subset(df, case==x)$mark,
-                                    # 1, if road markings
-                                    trees = subset(df, case==x)$trees,
-                                    # 1, if trees
-                                    bollards = subset(df, case==x)$bollards,
-                                    # 1, if bollards
-                                    width = subset(df, case==x)$width,
-                                    # width of the pavement
-                                    cross_y = subset(df, case==x)$cross_y,
-                                    # 1, existence of courtesy crossings
-                                    s_width = subset(df, case==x)$s_width,
-                                    # 1, sidewalk width
-                                    # inters = subset(df, case==x)$inters, no use of this variable
-                                    bench = subset(df, case==x)$bench,
-                                    # 1, if benches
-                                    obst = subset(df, case==x)$obst,
-                                    # 1, if obstacles
-                                    lev_segr = subset(df, case==x)$lev_segr),
-                                    vis_segr = subset(df, case==x)$vis_segr) # 1, if level segregatioN
+  # datafr<-cbind(datafr, data.frame(shared = subset(df, case==x)$shared,
+  #                                  # 1, if shared space, only one section
+  #                                  oneway = subset(df, case==x)$oneway,
+  #                                  lanes = subset(df, case==x)$lanes,
+  #                                  # 1, if oneway road
+  #                                  # park = subset(df, case==x)$park, # no use, all section with parking
+  #                                  mark = subset(df, case==x)$mark,
+  #                                  # 1, if road markings
+  #                                  trees = subset(df, case==x)$trees,
+  #                                  # 1, if trees
+  #                                  bollards = subset(df, case==x)$bollards,
+  #                                  # 1, if bollards
+  #                                  width = subset(df, case==x)$width,
+  #                                  # width of the pavement
+  #                                  cross_y = subset(df, case==x)$cross_y,
+  #                                  # 1, existence of courtesy crossings
+  #                                  s_width = subset(df, case==x)$s_width,
+  #                                  # 1, sidewalk width
+  #                                  # inters = subset(df, case==x)$inters, no use of this variable
+  #                                  bench = subset(df, case==x)$bench,
+  #                                  # 1, if benches
+  #                                  obst = subset(df, case==x)$obst,
+  #                                  # 1, if obstacles
+  #                                  lev_segr = subset(df, case==x)$lev_segr),
+  #                                  vis_segr = subset(df, case==x)$vis_segr) # 1, if level segregatioN
   return(datafr)}
 
 traf<-datafr_fun(gerdat,'fsbr', 'dats1', 20) # fsbr 20 km/h
@@ -123,8 +125,8 @@ traf$maxact<-ifelse(traf$case == 'fsbr', 7, ifelse(traf$case == 'lsho', 3,
                                                           ifelse(traf$case == 'shnaf', 6.5, 0))))
 
 traf$totspace <- (traf$minsafe + traf$maxact + traf$circ)
-#traf$shvehspace <- traf$circ/traf$totspace
-#traf$shpedspace <- 1 - traf$shvehspace
+traf$shvehspace <- traf$circ/traf$totspace
+traf$shpedspace <- 1 - traf$shvehspace
 
 
 
@@ -135,10 +137,10 @@ describe(subset(traf, case=='lsho')$speed)
 describe(subset(traf, case=='mke')$speed)
 describe(subset(traf, case=='shnaf')$speed)
 
-#describe(subset(traf, case=='fsbr')$shpedspace)
-#describe(subset(traf, case=='lsho')$shpedspace)
-#describe(subset(traf, case=='mke')$shpedspace)
-#describe(subset(traf, case=='shnaf')$shpedspace)
+describe(subset(traf, case=='fsbr')$shpedspace)
+describe(subset(traf, case=='lsho')$shpedspace)
+describe(subset(traf, case=='mke')$shpedspace)
+describe(subset(traf, case=='shnaf')$shpedspace)
 
 describe(subset(traf, case=='fsbr')$ped/30)
 describe(subset(traf, case=='lsho')$ped/30)
@@ -155,10 +157,10 @@ describe(subset(traf, case=='lsho')$cross/30)
 describe(subset(traf, case=='mke')$cross/30)
 describe(subset(traf, case=='shnaf')$cross/30)
 
-#describe(subset(traf, case=='fsbr')$shped)
-#describe(subset(traf, case=='lsho')$shped)
-#describe(subset(traf, case=='mke')$shped)
-#describe(subset(traf, case=='shnaf')$shped)
+describe(subset(traf, case=='fsbr')$shped)
+describe(subset(traf, case=='lsho')$shped)
+describe(subset(traf, case=='mke')$shped)
+describe(subset(traf, case=='shnaf')$shped)
 
 describe(subset(traf, case=='fsbr')$pedcross)
 describe(subset(traf, case=='lsho')$pedcross)
@@ -219,6 +221,9 @@ p101<-ggplot(traf, aes(y=dspeed, x=as.factor(cross/30))) +
       scale_x_discrete(name ="Pedestrian crossing in ped/2 minutes")
 
 ggarrange(p101, p100, ncol = 2)
+setwd(dirname(getActiveDocumentContext()$path))
+ggsave('figureplots/timeplots_all.png', plot = last_plot(), device = 'png', height = 7.95, width = 10.72, units = 'in')
+
 
 p11<-ggplot(traf, aes(x=as.factor(case), y = pedcross, fill=case)) + geom_boxplot() + 
    geom_point(size = 0.9, alpha = .3, position = position_jitter(seed = 1, width = .2)) +
@@ -268,6 +273,8 @@ p4<-ggplot(traf, aes(y=comply, x=dped, color=case))+ geom_point(size = 0.90)+ th
 #  scale_x_continuous(name = 'Pedestrin crossing rate in cross/ped')
 
 ggarrange(p11, p22, p1, p3, p2, p4, ncol=2, nrow=3, common.legend = TRUE, legend="bottom")
+setwd(dirname(getActiveDocumentContext()$path))
+ggsave('figureplots/boxscatterplots_all.png', plot = last_plot(), device = 'png', height = 8.60, width = 6.71, units = 'in')
 
 # end up with two important parameters: compliance rate and crossings per pedestrian
 # two different forces, comparison and analysis.
@@ -286,6 +293,7 @@ boxpl_fun<-function(df, x, y1, y2, text){
 } # box plot function to see visually the impact of each road design feature
 
 # Bollards, yer or no
+# FIX FIX PROBLEM WITH THE DESIGN
 mean(subset(traf, bollards == 1)$pedcross) - mean(subset(traf, bollards == 0)$pedcross) # 0.10 more crossings per pedestrian
 wilcox.test(subset(traf, bollards == 1)$pedcross, subset(traf, bollards == 0)$pedcross, exact=FALSE, paired=FALSE) # significance
 mean(subset(traf, bollards == 1)$comply) - mean(subset(traf, bollards == 0)$comply)
@@ -329,26 +337,36 @@ mean(subset(traf, obst == 1)$comply) - mean(subset(traf, obst == 0)$comply)
 wilcox.test(subset(traf, obst == 1)$comply, subset(traf, obst == 0)$comply, exact=FALSE, paired=FALSE) # significance
 boxpl_fun(traf, obst, pedcross, comply, '1, if there are obstacles')
 
-
+#################
 
 # HOT HOT HOT HOT FINALLY
 traf$pedcross2<-1/exp(traf$pedcross)
 traf$dflow2 <- traf$dflow/100
 traf$dped2 <- traf$dped/100
-model77<-lm(pedcross2 ~ dflow2 + dped + shcirc + shminsafe, data = traf[-c(228, 226, 196, 70, 31, 59, 46),])
+model77<-lm(pedcross2 ~ dflow + dped + shcirc + shminsafe, data = traf[-c(228, 226, 196, 70, 31, 59, 46),])
 summary(model77)
-ols_vif_tol(model77)
+# ols_vif_tol(model77)
 gvlma(model77)
 par(mfrow=c(2,2))
 plot(model77, pch=20)
+# SAVE THE PLOTS TO GIVE THE FINAL RESULTS
 
-model88<-lm(comply ~ dflow2 + dped2 + shcirc + shminsafe, data = traf[-c(250, 170, 157),])
+model88<-lm(comply ~ dflow + dped + shcirc + shminsafe, data = traf[-c(250, 170, 157),])
 summary(model88)
-ols_vif_tol(model88)
+# ols_vif_tol(model88)
 gvlma(model88)
 par(mfrow=c(2,2))
 plot(model88, pch=20)
 # HOT HOT HOT HOT FINALLY
+
+# output of models
+setwd(dirname(getActiveDocumentContext()$path)) 
+sink("models/model77.txt")
+print(summary(model77))
+sink()
+sink("models/model88.txt")
+print(summary(model88))
+sink()
 
 # Simulation of these models: model88 and model77
 magicpl<-function(circ, saf, act, name) {
@@ -397,4 +415,4 @@ ggarrange(magicpl(15, 0, 0, 's1: circ: 15%; saf: 0%; act: 0%'),
           magicpl(4.5, 4.5, 6, 's4: circ: 30%; saf: 30%; act: 40%'),
           magicpl(0, 0, 15, 's5: circ: 0%; saf: 0%; act: 100%'),
           ncol=1, nrow=5)
-
+ggsave('figureplots/model_simu_results_all.png', plot = last_plot(), device = 'png', height = 8.22, width = 7.04, units = 'in')
